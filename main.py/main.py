db4ad8a72c7dd0f914a9107742dee10ae574423b
Robert Heslar <robert.heslar@freseniusmedicalcare.com>
2025-08-25 14:13:49 -0400

    Create main.py
    add main.py

---
+++ main.py/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Visual Studio Code Editor
+
+This is a script file that is testing some features of Python to check the 
+compatability of Python to the CSDmanager application.
+
+"""
