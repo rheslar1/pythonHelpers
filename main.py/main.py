a459232ff8cf15c17386e717d0f60ba88b95734c
Robert Heslar <robert.heslar@freseniusmedicalcare.com>
2025-08-25 14:14:44 -0400

    Delete main.py

--- main.py/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-Visual Studio Code Editor
-
-This is a script file that is testing some features of Python to check the 
-compatability of Python to the CSDmanager application.
-
-"""
